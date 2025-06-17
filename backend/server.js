70b24aba9dc92659491aed8d62a0f89416265482
Preet2099x <preet2099x@gmail.com>
2025-06-18 04:42:17 +0530

    Added login and signup api

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import loginRoutes from './routes/loginRoutes.js'
+import signupRoutes from './routes/signupRoutes.js'
 
 const app = express()
 const PORT = 8000
+
+app.use(express.json())
+
 app.get('/' , (req,res) => {
     res.send('CinemaBoxd backend is running!')
 })
+
+app.use('/api',loginRoutes)
+app.use('/api',signupRoutes)
 
 app.listen(PORT | 8000 , () => {
     console.log(`Server is running at PORT:${PORT}`)
